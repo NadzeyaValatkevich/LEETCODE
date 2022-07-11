429bdbce486efae5b8e94a7b2c38dd5dff8ce426
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-07-11 13:30:40 +0300

    added task: Find All Numbers Disappeared in an Array

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 //task 1
 //217.Contains Duplicate
 
-var containsDuplicate = function (nums) {
-    const res = {};
-    nums.forEach((elem) => {
-        res[elem] = 1
-    });
-    return (Object.keys(res).length < nums.length)
-};
-
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+// var containsDuplicate = function (nums) {
+//     const res = {};
+//     nums.forEach((elem) => {
+//         res[elem] = 1
+//     });
+//     return (Object.keys(res).length < nums.length)
+// };
+//
+// console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
 
 
 //task 2
 //268. Missing Number
 
-var missingNumber = function (nums) {
-    let n = nums.length;
-    let newArray = [];
+// var missingNumber = function (nums) {
+//     let n = nums.length;
+//     let newArray = [];
+//     function f(n) {
+//         if (n < 0) {
+//             return
+//         } else {
+//             newArray.unshift(n);
+//             f(n - 1)
+//         }
+//     };
+//     f(n);
+//     for(let i of newArray) {
+//         if(!nums.includes(i)){
+//             return i
+//         }
+//     }
+// };
+//
+// console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
+
+
+//task 3
+//448Find All Numbers Disappeared in an Array
+var findDisappearedNumbers = function(nums) {
+    const n = nums.length;
+    const newArray = [];
     function f(n) {
-        if (n < 0) {
-            return
-        } else {
-            newArray.unshift(n);
+        if(n <= 0) return
+        else {
+            newArray.unshift(n)
             f(n - 1)
         }
-    };
-    f(n);
-    for(let i of newArray) {
-        if(!nums.includes(i)){
-            return i
-        }
     }
-};
+    f(n);
+    const result = newArray.filter((elem) => !nums.includes(elem));
+    return result
+    };
 
-console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
+console.log(findDisappearedNumbers([1,1]))
 
