ec4ab397ff3f696880c651280261848ce808785f
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-07-15 19:46:10 +0300

    added task: Single Number

--- index.js
+++ index.js
@@ -39,20 +39,39 @@
 
 //task 3
 //448Find All Numbers Disappeared in an Array
-var findDisappearedNumbers = function(nums) {
-    const n = nums.length;
-    const newArray = [];
-    function f(n) {
-        if(n <= 0) return
-        else {
-            newArray.unshift(n)
-            f(n - 1)
+// var findDisappearedNumbers = function(nums) {
+//     const n = nums.length;
+//     const newArray = [];
+//     function f(n) {
+//         if(n <= 0 || n >= 10**5) return
+//         else {
+//             newArray.unshift(n)
+//             f(n - 1)
+//         }
+//     }
+//     f(n);
+//     const result = newArray.filter((elem) => !nums.includes(elem));
+//     return result
+//     };
+//
+// console.log(findDisappearedNumbers([1,1]))
+
+//task4
+//136 Single Number
+
+var singleNumber = function (nums) {
+    const obj = {};
+    for (let i of nums) {
+        if (!obj[i]) {
+            obj[i] = 1
+        } else {
+            obj[i] += 1
         }
     }
-    f(n);
-    const result = newArray.filter((elem) => !nums.includes(elem));
-    return result
-    };
+    for (let i in obj) {
+        if (obj[i] === 1) return i
+    }
+};
 
-console.log(findDisappearedNumbers([1,1]))
+console.log(singleNumber([2, 2, 1]))
 
