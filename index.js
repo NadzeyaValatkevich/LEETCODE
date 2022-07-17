0c81ee5d848038d85dc26cea53ae8c6b7bc73b0a
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-07-17 13:11:17 +0300

    added task: 121. Best Time to Buy and Sell Stock

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 //task 1
 //217.Contains Duplicate
 
-// var containsDuplicate = function (nums) {
-//     const res = {};
-//     nums.forEach((elem) => {
-//         res[elem] = 1
-//     });
-//     return (Object.keys(res).length < nums.length)
-// };
-//
-// console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+var containsDuplicate = function (nums) {
+    const res = {};
+    nums.forEach((elem) => {
+        res[elem] = 1
+    });
+    return (Object.keys(res).length < nums.length)
+};
+
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
 
 
 //task 2
 //268. Missing Number
 
-// var missingNumber = function (nums) {
-//     let n = nums.length;
-//     let newArray = [];
-//     function f(n) {
-//         if (n < 0) {
-//             return
-//         } else {
-//             newArray.unshift(n);
-//             f(n - 1)
-//         }
-//     };
-//     f(n);
-//     for(let i of newArray) {
-//         if(!nums.includes(i)){
-//             return i
-//         }
-//     }
-// };
-//
-// console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
+var missingNumber = function (nums) {
+    let n = nums.length;
+    let newArray = [];
+
+    function f(n) {
+        if (n < 0) {
+            return
+        } else {
+            newArray.unshift(n);
+            f(n - 1)
+        }
+    };
+    f(n);
+    for (let i of newArray) {
+        if (!nums.includes(i)) {
+            return i
+        }
+    }
+};
+
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
 
 
 //task 3
 //448Find All Numbers Disappeared in an Array
-// var findDisappearedNumbers = function(nums) {
-//     const n = nums.length;
-//     const newArray = [];
-//     function f(n) {
-//         if(n <= 0 || n >= 10**5) return
-//         else {
-//             newArray.unshift(n)
-//             f(n - 1)
-//         }
-//     }
-//     f(n);
-//     const result = newArray.filter((elem) => !nums.includes(elem));
-//     return result
-//     };
-//
-// console.log(findDisappearedNumbers([1,1]))
+var findDisappearedNumbers = function (nums) {
+    const n = nums.length;
+    const newArray = [];
+
+    function f(n) {
+        if (n <= 0 || n >= 10 ** 5) return
+        else {
+            newArray.unshift(n)
+            f(n - 1)
+        }
+    }
+
+    f(n);
+    const result = newArray.filter((elem) => !nums.includes(elem));
+    return result
+};
+
+console.log(findDisappearedNumbers([1, 1]))
 
 //task4
 //136 Single Number
@@ -75,3 +78,18 @@ var singleNumber = function (nums) {
 
 console.log(singleNumber([2, 2, 1]))
 
+//task 5
+//121. Best Time to Buy and Sell Stock
+
+var maxProfit = function (prices) {
+    let profit = 0;
+    let min = prices[0];
+    for (let i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        let currprofit = prices[i] - min;
+        profit = Math.max(currprofit, profit)
+    }
+    return profit
+};
+
+console.log(maxProfit([7,1,5,3,6,4]))
